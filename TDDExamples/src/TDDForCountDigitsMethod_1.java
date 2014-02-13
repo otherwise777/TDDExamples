0494ea49550c2369266104423f0d85ab1f0f3c10
s133781 <s133781@S133781.campus.tue.nl>
2014-02-13 22:55:25 +0100

    fixed nothing

---
+++ TDDExamples/src/TDDForCountDigitsMethod_1.java
@@ -0,0 +1,30 @@
+////# BEGIN SKELETON
+///**
+// * Illustrates Test-Driven Development of a single method.
+// *
+//<!--//# BEGIN TODO Name, student id, and date-->
+//<p><font color="red"><b>Replace this line</b></font></p>
+//<!--//# END TODO -->
+//*/
+//// -----8<----- cut line -----8<-----
+//public class TDDForCountDigitsMethod {
+//
+//    /**
+//     * Counts the digits of a number.
+//     * This concerns a non-negative integer, assumed to be
+//     * written in positional notation without leading zeroes.
+//     * Robust version.
+//     *
+//<!--//# BEGIN TODO Contract-->
+//<p><font color="red"><b>Replace this line</b></font></p>
+//<!--//# END TODO-->
+//     */
+//    public static int countDigits(long n, long r)
+//            throws IllegalArgumentException {
+////# BEGIN TODO Implementation
+//// Replace this line
+////# END TODO
+//    }
+//
+//}
+////# END SKELETON
---
+++ TDDExamples/test/TDDForCountDigitsMethodTest_1.java
@@ -0,0 +1,36 @@
+////# BEGIN SKELETON
+//import static org.junit.Assert.*;
+//import org.junit.Test;
+//
+///**
+// * Test cases for countDigits method, of class TDDForCountDigitsMethod.
+// *
+//<!--//# BEGIN TODO Name, student id, and date-->
+//<p><font color="red"><b>Replace this line</b></font></p>
+//<!--//# END TODO -->
+// */
+//// -----8<----- cut line -----8<-----
+//public class TDDForCountDigitsMethodTest {
+//
+//    /** Subject Under Test.  Only static members are used. */
+//    private static final TDDForCountDigitsMethod SUT = null;
+//
+//    /**
+//     * Invokes countDigits(n, r) and checks result against expectation.
+//     *
+//     * @param n  the number whose digits are counted
+//     * @param r  the radix
+//     * @param expResult  the expected result
+//     */
+//    private void checkCountDigits(long n, long r, int expResult) {
+//        System.out.println("countDigits(" + n + ", " + r + ")");
+//        int result = SUT.countDigits(n, r);
+//        assertEquals("result", expResult, result);
+//    }
+//
+////# BEGIN TODO Test cases
+//// Replace this line
+////# END TODO
+//
+//}
+////# END SKELETON
