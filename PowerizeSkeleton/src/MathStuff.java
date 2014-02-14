79f5b033f28a734d8605d7302a862b8cb6068d49
s133781 <s133781@S133781.campus.tue.nl>
2014-02-14 10:48:44 +0100

    added project

---
+++ PowerizeSkeleton/src/MathStuff.java
@@ -0,0 +1,125 @@
+//# BEGIN SKELETON
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Library with static mathematical functions.
+ *
+<!--//# BEGIN TODO Name, student id, and date-->
+<p><font color="red"><b>Replace this line</b></font></p>
+<!--//# END TODO-->
+*/
+// -----8<----- cut line -----8<-----
+public abstract class MathStuff {
+
+    /**
+     * Returns exponentiation, taking care of overflow.
+     *
+     * @param a  the base
+     * @param b  the exponent
+     * @pre {@code 0 <= a && 0 <= b}
+     * @return {@code a ^ b} if {@code a ^ b <= Integer.MAX_VALUE}
+     *      else {@code Long.MAX_VALUE}
+     * @throws IllegalArgumentException  if precondition violated
+     */
+    public static long power(int a, int b) throws IllegalArgumentException {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("power: negative argument");
+        }
+        // 0 <= a && 0 <= b
+        long x = a; // see invariant
+        int k = b; // see invariant
+        long result = 1L; // see invariant
+
+        // invariant: 0 <= k <= b && result * x^k == a^b
+        while (k != 0) {
+            if (k % 2 == 0) { // even exponent
+                if (x <= Integer.MAX_VALUE) {
+                    x *= x;
+                } else {
+                    x = Long.MAX_VALUE;
+                }
+                k /= 2;
+            } else { // odd exponent
+                if (result <= Integer.MAX_VALUE && x <= Integer.MAX_VALUE) {
+                    result *= x;
+                } else {
+                    result = Long.MAX_VALUE;
+                }
+                k -= 1;
+            }
+            // invariant holds again, k has decreased
+        }
+        // k == 0, hence result == a^b
+
+        if (result > Integer.MAX_VALUE) {
+            return Long.MAX_VALUE;
+        }
+        return result;
+    }
+
+    /**
+     * Record containing a base and an exponent.
+     *
+     * @inv {@code 0 <= base && 0 <= exponent}
+     */
+    public static class Power { // BEGIN RECORD TYPE
+
+        /** The base. */
+        public int base;
+
+        /** The exponent. */
+        public int exponent;
+
+        /**
+         * Constructs a Power with given base and exponent.
+         *
+         * @param base  the base
+         * @param exponent  the exponent
+         * @pre {@code 0 <= base && 0 <= exponent}
+         * @post {@code \result.base == base && \result.exponent == exponent}
+         */
+        public Power(int base, int exponent) {
+            this.base = base;
+            this.exponent = exponent;
+        }
+
+    } // END RECORD TYPE
+
+    /**
+     * Returns exponentiation.
+     *
+     * @param p  the base and exponent
+     * @pre {@code p != null}
+     * @return {@code power(p.base, p.exponent)}
+     * @throws IllegalArgumentException  if precondition violated
+     */
+    public static long power(Power p) throws IllegalArgumentException {
+        return power(p.base, p.exponent);
+    }
+
+    /**
+     * Writes a number as a power with maximal exponent.
+     *
+     * @param n  the number to 'powerize'
+     * @return  power decomposition of {@code n} with maximal exponent
+     * @throws IllegalArgumentException  if precondition violated
+     * @pre {@code 2 <= n}
+     * @post {@code n == power(\result) &&
+     *     (\forall int b, int e;
+     *      2 <= b && 1 <= e && n == b ^ e;
+     *      e <= \result.exponent)}
+     */
+    public static Power powerize(int n) throws IllegalArgumentException {
+//# BEGIN TODO Implementation of powerize
+// Replace this line
+//# END TODO
+        return null;
+    }
+
+//# BEGIN TODO Contracts and implementations of auxiliary functions.
+// Replace this line
+//# END TODO
+
+}
+//# END SKELETON
---
+++ PowerizeSkeleton/test/MathStuffTestAuxiliary.java
@@ -0,0 +1,23 @@
+//# BEGIN SKELETON
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Test cases for auxiliary methods in {@code MathStuff}.
+ *
+<!--//# BEGIN TODO Name, student id, and date-->
+<p><font color="red"><b>Replace this line</b></font></p>
+<!--//# END TODO-->
+*/
+// -----8<----- cut line -----8<-----
+public class MathStuffTestAuxiliary {
+
+//# BEGIN TODO Test cases for auxiliary functions
+// Replace this line
+//# END TODO
+
+}
+//# END SKELETON
